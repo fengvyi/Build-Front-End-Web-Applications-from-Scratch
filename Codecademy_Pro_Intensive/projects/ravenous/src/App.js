30608a606717c48fa914fccb35f3388209ee2dde
fengvyi <zefengsong@gwmail.gwu.edu>
2018-04-29 17:45:27 -0400

    Update ravenous

--- Codecademy_Pro_Intensive/projects/ravenous/src/App.js
+++ Codecademy_Pro_Intensive/projects/ravenous/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <h1>Zefeng</h1>
       <h1>ravenous</h1>
         <SearchBar />
         <BusinessList />
