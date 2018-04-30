81b372dd2d2dc0b7edb28cb999199200df9e1fcf
fengvyi <zefengsong@gwmail.gwu.edu>
2018-04-29 20:09:11 -0400

    Add Passing Information to Components feature to Ravenous

--- Codecademy_Pro_Intensive/projects/ravenous/src/App.js
+++ Codecademy_Pro_Intensive/projects/ravenous/src/App.js
@@ -4,14 +4,33 @@ import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
-class App extends Component {
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
 
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+];
+
+class App extends Component {
   render() {
     return (
       <div className="App">
       <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
--- Codecademy_Pro_Intensive/projects/ravenous/src/components/BusinessList/BusinessList.js
+++ Codecademy_Pro_Intensive/projects/ravenous/src/components/BusinessList/BusinessList.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
-class BusinessList extends React.Component {
 
+class BusinessList extends React.Component {
 
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {
+          this.props.businesses.map(business =>
+          {
+            return <Business business={business} />;
+          })
+        }
       </div>
     );
   }
